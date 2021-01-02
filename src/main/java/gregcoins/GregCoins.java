b7ba283d69d09633a2a56ca5ce50d4962eb893a1
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2021-01-01 18:12:19 -0800

    h
    h

--- src/main/java/gregcoins/GregCoins.java
+++ src/main/java/gregcoins/GregCoins.java
@@ -32,7 +32,7 @@ import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.lang.Math;
 
-@Mod(modid = GregCoins.MODID, acceptedMinecraftVersions = "[1.12, 1.13)", dependencies = "required-after:ftbmoney", guiFactory = "gregcoins.GuiFactory")
+@Mod(modid = GregCoins.MODID, acceptedMinecraftVersions = "[1.12, 1.13)", dependencies = "required-after:ftbmoney, gregtech", guiFactory = "gregcoins.GuiFactory")
 @Mod.EventBusSubscriber
 public class GregCoins
 {
@@ -41,122 +41,31 @@ public class GregCoins
     public static Configuration config;
 
 // GT Coins
-    public static int[] coinCupronickelValues;
-    public static int[] coinSilverValues;
-    public static int[] coinGoldValues;
-    public static int[] coinPlatinumValues;
-    public static int[] coinOsmiumValues;
-    public static int[] coinNaquadahValues;
-    public static int[] coinDarmstadtiumValues;
-    
-    @GameRegistry.ObjectHolder("gregcoins:coin_cupronickel")
-    public static Item COIN_CUPRONICKEL;
-    @GameRegistry.ObjectHolder("gregcoins:coin_silver")
-    public static Item COIN_SILVER;
-    @GameRegistry.ObjectHolder("gregcoins:coin_gold")
-    public static Item COIN_GOLD;
-    @GameRegistry.ObjectHolder("gregcoins:coin_platinum")
-    public static Item COIN_PLATINUM;
-    @GameRegistry.ObjectHolder("gregcoins:coin_osmium")
-    public static Item COIN_OSMIUM;
-    @GameRegistry.ObjectHolder("gregcoins:coin_naquadah")
-    public static Item COIN_NAQUADAH;
-    @GameRegistry.ObjectHolder("gregcoins:coin_darmstadtium")
-    public static Item COIN_DARMSTADTIUM;
+    @GameRegistry.ObjectHolder("gregtech:meta_item_1:32001")
+    public static Item GT_CUPRONICKEL;
+    @GameRegistry.ObjectHolder("gregtech:meta_item_1:32002")
+    public static Item GT_SILVER;
+    @GameRegistry.ObjectHolder("gregtech:meta_item_1:32003")
+    public static Item GT_GOLD;
+    @GameRegistry.ObjectHolder("gregtech:meta_item_1:32004")
+    public static Item GT_PLATINUM;
+    @GameRegistry.ObjectHolder("gregtech:meta_item_1:32005")
+    public static Item GT_OSMIUM;
+    @GameRegistry.ObjectHolder("gregtech:meta_item_1:32006")
+    public static Item GT_NAQUADAH;
+    @GameRegistry.ObjectHolder("gregtech:meta_item_1:32007")
+    public static Item GT_DARMSTADTIUM;
+
+    public static final ItemStack cupronickelItemStack = new ItemStack(GT_CUPRONICKEL);
+    public static final ItemStack silverItemStack = new ItemStack(GT_SILVER);
+    public static final ItemStack goldItemStack = new ItemStack(GT_GOLD);
+    public static final ItemStack platinumItemStack = new ItemStack(GT_PLATINUM);
+    public static final ItemStack osmiumItemStack = new ItemStack(GT_OSMIUM);
+    public static final ItemStack naquahahItemStack = new ItemStack(GT_NAQUADAH);
+    public static final ItemStack darmstadtiumItemStack = new ItemStack(GT_DARMSTADTIUM);
+
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        config = new Configuration(event.getSuggestedConfigurationFile());
-
-        loadConfig();
-
         MinecraftForge.EVENT_BUS.register(this);
     }
-
-    @SubscribeEvent
-    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event)
-    {
-        if(event.getModID().equalsIgnoreCase(GregCoins.MODID))
-        {
-            loadConfig();
-        }
-    }
-
-    public static void loadConfig() {
-        coinCupronickelValues = config.get("coins", "coinCupronickelValues", new int[] { 1 }, "List of Cupronickel coin denominations that show up in JEI. The base values are original to GTCE.").getIntList();
-        coinSilverValues = config.get("coins", "coinSilverValues", new int[] { 8 }, "List of Silver coin denominations that show up in JEI. The base values are original to GTCE.").getIntList();
-        coinGoldValues = config.get("coins", "coinGoldValues", new int[] { 64 }, "List of Gold coin denominations that show up in JEI. The base values are original to GTCE.").getIntList();
-        coinPlatinumValues = config.get("coins", "coinPlatinumValues", new int[] { 512 }, "List of Platinum coin denominations that show up in JEI. The base values are original to GTCE.").getIntList();
-        coinOsmiumValues = config.get("coins", "coinOsmiumValues", new int[] { 4096 }, "List of Osmium coin denominations that show up in JEI. The base values are original to GTCE.").getIntList();
-        coinNaquadahValues = config.get("coins", "coinNaquadahValues", new int[] { 32768 }, "List of Naquadah coin denominations that show up in JEI. The base values are original to GTCE.").getIntList();
-        coinDarmstadtiumValues = config.get("coins", "coinDarmstadtiumValues", new int[] { 262144 }, "List of Darmstadtium coin denominations that show up in JEI. The base values are original to GTCE.").getIntList();
-       
-        if (config.hasChanged()) {
-            config.save();
-        }
-    }
-
-    @SubscribeEvent
-    public void registerItems(RegistryEvent.Register<Item> event) {
-
-        event.getRegistry().register(COIN_CUPRONICKEL = new ItemCoin() {
-            @Override
-            public int[] getDenominations() {
-                return coinCupronickelValues;
-            }
-        }.setRegistryName(new ResourceLocation(GregCoins.MODID,"coin_cupronickel")).setUnlocalizedName("coin_cupronickel").setCreativeTab(CreativeTabs.MATERIALS));
-        event.getRegistry().register(COIN_SILVER = new ItemCoin() {
-            @Override
-            public int[] getDenominations() {
-                return coinSilverValues;
-            }
-        }.setRegistryName(new ResourceLocation(GregCoins.MODID,"coin_silver")).setUnlocalizedName("coin_silver").setCreativeTab(CreativeTabs.MATERIALS));
-        event.getRegistry().register(COIN_GOLD = new ItemCoin() {
-            @Override
-            public int[] getDenominations() {
-                return coinGoldValues;
-            }
-        }.setRegistryName(new ResourceLocation(GregCoins.MODID,"coin_gold")).setUnlocalizedName("coin_gold").setCreativeTab(CreativeTabs.MATERIALS));
-        event.getRegistry().register(COIN_PLATINUM = new ItemCoin() {
-            @Override
-            public int[] getDenominations() {
-                return coinPlatinumValues;
-            }
-        }.setRegistryName(new ResourceLocation(GregCoins.MODID,"coin_platinum")).setUnlocalizedName("coin_platinum").setCreativeTab(CreativeTabs.MATERIALS));
-        event.getRegistry().register(COIN_OSMIUM = new ItemCoin() {
-            @Override
-            public int[] getDenominations() {
-                return coinOsmiumValues;
-            }
-        }.setRegistryName(new ResourceLocation(GregCoins.MODID,"coin_osmium")).setUnlocalizedName("coin_osmium").setCreativeTab(CreativeTabs.MATERIALS));
-        event.getRegistry().register(COIN_NAQUADAH = new ItemCoin() {
-            @Override
-            public int[] getDenominations() {
-                return coinNaquadahValues;
-            }
-        }.setRegistryName(new ResourceLocation(GregCoins.MODID,"coin_naquadah")).setUnlocalizedName("coin_naquadah").setCreativeTab(CreativeTabs.MATERIALS));
-        event.getRegistry().register(COIN_DARMSTADTIUM = new ItemCoin() {
-            @Override
-            public int[] getDenominations() {
-                return coinDarmstadtiumValues;
-            }
-        }.setRegistryName(new ResourceLocation(GregCoins.MODID,"coin_darmstadtium")).setUnlocalizedName("coin_darmstadtium").setCreativeTab(CreativeTabs.MATERIALS));
-    }
-
-    @SideOnly(Side.CLIENT)
-    @SubscribeEvent
-    public void registerModels(ModelRegistryEvent event) {
-        registerItemModel(COIN_CUPRONICKEL, 0, "inventory");
-        registerItemModel(COIN_SILVER, 0, "inventory");
-        registerItemModel(COIN_GOLD, 0, "inventory");
-        registerItemModel(COIN_PLATINUM, 0, "inventory");
-        registerItemModel(COIN_OSMIUM, 0, "inventory");
-        registerItemModel(COIN_NAQUADAH, 0, "inventory");
-        registerItemModel(COIN_DARMSTADTIUM, 0, "inventory");
-    }
-
-    @SideOnly(Side.CLIENT)
-    public static void registerItemModel(@Nonnull Item item, int meta, String variant) {
-        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), variant));
-    }
-}
--- src/main/java/gregcoins/ItemCoin.java
+++ src/main/java/gregcoins/ItemCoin.java
@@ -12,65 +12,68 @@ import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 
 public abstract class ItemCoin extends Item {
-    private long getValue(ItemStack stack) {
-        if (stack.getItem() != this)
-            return 0;
-        NBTTagCompound compound = stack.getTagCompound();
-        if (compound == null)
-            return 0;
-        else
-            return compound.getLong("value");
-    }
-
-    private void setValue(ItemStack stack, long value) {
-        NBTTagCompound compound = stack.getTagCompound();
-        if(compound == null)
-            compound = new NBTTagCompound();
-        compound.setLong("value",value);
-        stack.setTagCompound(compound);
-    }
-
-    private ItemStack ofValue(long value) {
-        ItemStack stack = new ItemStack(this);
-        setValue(stack,value);
-        return stack;
-    }
-
-    public abstract int[] getDenominations();
-
-    @Override
-    public boolean getHasSubtypes() {
-        return true;
-    }
-
-    @Override
-    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
-        if (this.isInCreativeTab(tab)) {
-            for (int denomination : getDenominations()) {
-                items.add(ofValue(denomination));
-            }
-        }
-    }
-
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer player, EnumHand hand) {
+
         ItemStack heldItem = player.getHeldItem(hand);
 
         int useCount = 1;
+
         if (player.isSneaking())
             useCount = heldItem.getCount();
 
-        long value = getValue(heldItem) * useCount;
-        FTBMoney.setMoney(player, FTBMoney.getMoney(player) + value);
-        player.sendStatusMessage(FTBMoney.moneyComponent(FTBMoney.getMoney(player)),true);
+        if(heldItem == GregCoins.cupronickelItemStack){
+            long coinValue = 1
+            long value = coinValue * useCount;
+            FTBMoney.setMoney(player, FTBMoney.getMoney(player) + value);
+            player.sendStatusMessage(FTBMoney.moneyComponent(FTBMoney.getMoney(player)),true);
+        }
+
+        if(heldItem == GregCoins.silverItemStack){
+            long coinValue = 8
+            long value = coinValue * useCount;
+            FTBMoney.setMoney(player, FTBMoney.getMoney(player) + value);
+            player.sendStatusMessage(FTBMoney.moneyComponent(FTBMoney.getMoney(player)),true);
+        }
+
+        if(heldItem == GregCoins.goldItemStack){
+            long coinValue = 64
+            long value = coinValue * useCount;
+            FTBMoney.setMoney(player, FTBMoney.getMoney(player) + value);
+            player.sendStatusMessage(FTBMoney.moneyComponent(FTBMoney.getMoney(player)),true);
+        }
+
+        if(heldItem == GregCoins.platinumItemStack){
+            long coinValue = 512
+            long value = coinValue * useCount;
+            FTBMoney.setMoney(player, FTBMoney.getMoney(player) + value);
+            player.sendStatusMessage(FTBMoney.moneyComponent(FTBMoney.getMoney(player)),true);
+        }
+
+        if(heldItem == GregCoins.osmiumItemStack){
+            long coinValue = 4096
+            long value = coinValue * useCount;
+            FTBMoney.setMoney(player, FTBMoney.getMoney(player) + value);
+            player.sendStatusMessage(FTBMoney.moneyComponent(FTBMoney.getMoney(player)),true);
+        }
+
+        if(heldItem == GregCoins.naquadahItemStack){
+            long coinValue = 32768
+            long value = coinValue * useCount;
+            FTBMoney.setMoney(player, FTBMoney.getMoney(player) + value);
+            player.sendStatusMessage(FTBMoney.moneyComponent(FTBMoney.getMoney(player)),true);
+        }
+
+        if(heldItem == GregCoins.darmstadtiumItemStack){
+            long coinValue = 262144
+            long value = coinValue * useCount;
+            FTBMoney.setMoney(player, FTBMoney.getMoney(player) + value);
+            player.sendStatusMessage(FTBMoney.moneyComponent(FTBMoney.getMoney(player)),true);
+        }
+
 
         heldItem.shrink(useCount);
 
         return new ActionResult<>(EnumActionResult.SUCCESS, heldItem);
-    }
-
-    @Override
-    public String getItemStackDisplayName(ItemStack stack) {
-        return I18n.translateToLocalFormatted(this.getUnlocalizedNameInefficiently(stack) + ".name", FTBMoney.moneyString(getValue(stack))).trim();
     }
 }
